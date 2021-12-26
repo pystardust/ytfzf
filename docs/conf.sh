f9505cd5936b5aecd6a45da773068eff176d8140
Euro <no>
2021-12-26 11:18:54 -0800

    docs: add explanation for small conf.sh

--- docs/conf.sh
+++ docs/conf.sh
@@ -1,5 +1,10 @@
 # This is a sample config file, refer to ytfzf(5) for more information
 
+# In the previous version of ytfzf this file had all the examples, with all defaults set,
+# this has been changed because it made it impossible for us to change default values that were broken or causing bugs,
+# as everyone used the default configuration file.
+# we are now going to only have this sample config file, and the ytfzf(5) manual, which has explanation of every variable and function that can be set.
+
 
 #Variables {{{
 video_pref="248+bestaudio/best"
